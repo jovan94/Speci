8642fc8463fd349aba9839af2e0bcaf56236cd26
student <student@521-11>
2016-10-21 13:17:14 +0200

    Test

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author student
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("test");
+        // TODO code application logic here
+    }
+}
