d5df092391612e965230f19d7757a482ab73552e
student <student@521-11>
2016-10-21 13:22:10 +0200

    let there be light

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -15,6 +15,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         System.out.println("test");
+        //lalalala
         // TODO code application logic here
     }
 }
